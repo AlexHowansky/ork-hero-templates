505bd3b0c60286be168e941e99438cc3488dea93
Alex Howansky <alex.howansky@gmail.com>
2025-03-11 16:28:31 -0400

    Moved blocks to a subdir and automated block detection.

--- build.php
+++ build.php
@@ -6,32 +6,20 @@ if ($argc < 2) {
     exit(1);
 }
 
-$output = file_get_contents('template.html') . str_replace(
-    [
-        '<!--CHARACTERISTICS_BLOCK-->',
-        '<!--SKILLS_BLOCK-->',
-        '<!--POWERS_BLOCK-->',
-        '<!--DEFENSE_BLOCK-->',
-        '<!--COMBAT_INFO_BLOCK-->',
-        '<!--COMBAT_LEVELS_BLOCK-->',
-        '<!--COMBAT_MANEAUVERS_BLOCK-->',
-        '<!--RANGE_MODIFIERS_BLOCK-->',
-        '/*CSS_BLOCK*/',
-        '/*JAVASCRIPT_BLOCK*/'
-    ],
-    [
-        file_get_contents('characteristics.html'),
-        file_get_contents('skills.html'),
-        file_get_contents('powers.html'),
-        file_get_contents('defense.html'),
-        file_get_contents('combat_info.html'),
-        file_get_contents('combat_levels.html'),
-        file_get_contents('combat_maneauvers.html'),
-        file_get_contents('range_modifiers.html'),
-        file_get_contents('page.css'),
-        file_get_contents('page.js'),
-    ],
-    file_get_contents('page.html')
+$output = file_get_contents('template.html') . file_get_contents('page.html');
+
+$blocks = glob('blocks/*.html');
+$output = str_replace(
+    array_map(fn(string $file): string => sprintf('<!--%s_BLOCK-->', strtoupper(basename($file, '.html'))), $blocks),
+    array_map(fn(string $file): string => file_get_contents($file), $blocks),
+    $output
+);
+
+$blocks = ['css', 'js'];
+$output = str_replace(
+    array_map(fn(string $block): string => sprintf('/*%s_BLOCK*/', strtoupper($block)), $blocks),
+    array_map(fn(string $block): string => file_get_contents(sprintf('page.%s', $block)), $blocks),
+    $output
 );
 
 printf("Wrote %d bytes to %s\n", file_put_contents($argv[1], $output), $argv[1]);
