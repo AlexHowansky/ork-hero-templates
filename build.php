a29344733e0030c493c88a8876ec3f15161071c7
Alex Howansky <alex.howansky@gmail.com>
2025-03-27 11:35:00 -0400

    Added HTML linting to build.

--- build.php
+++ build.php
@@ -1,12 +1,13 @@
 #!/usr/bin/env php
 <?php
 
-foreach (glob('layouts/*.html') as $layoutHtml) {
-    $layoutXml = preg_replace('/\.html$/', '.xml', $layoutHtml);
-    $output =
-        preg_replace(['/^\s+/m', '/\n/'], '', file_get_contents($layoutXml) . file_get_contents('template.xml')) .
-        file_get_contents('page.html');
-    $output = str_replace('<!--LAYOUT-->', file_get_contents($layoutHtml), $output);
+use Dom\HTMLDocument;
+
+libxml_use_internal_errors(true);
+
+foreach (glob('layouts/*.html') as $layoutFileHtml) {
+    $output = file_get_contents('page.html');
+    $output = str_replace('<!--LAYOUT-->', file_get_contents($layoutFileHtml), $output);
     $output = str_replace('/*CSS*/', '<!--CAMPAIGN_USE--><!--/CAMPAIGN_USE-->', $output);
     $blocks = glob('blocks/*.html');
     $output = str_replace(
@@ -17,7 +18,22 @@ foreach (glob('layouts/*.html') as $layoutHtml) {
     $outputFile = sprintf(
         '%s/Ork-%s.hde',
         rtrim($argv[1] ?? './dist' ?: './dist', '/'),
-        ucfirst(basename($layoutHtml, '.html'))
+        ucfirst(basename($layoutFileHtml, '.html'))
     );
+    HTMLDocument::createFromString($output);
+    $errors = libxml_get_errors();
+    if (count($errors) > 0) {
+        printf("Errors detected while processing %s:\n", $outputFile);
+        foreach ($errors as $error) {
+            printf("  %s\n", $error->message);
+        }
+        libxml_clear_errors();
+    } else {
+        $output = preg_replace(
+            ['/^\s+/m', '/\n/'],
+            '',
+            file_get_contents(preg_replace('/\.html$/', '.xml', $layoutFileHtml)) . file_get_contents('template.xml')
+        ) . $output;
+    }
     printf("Wrote %d bytes to %s\n", file_put_contents($outputFile, $output), $outputFile);
 }
